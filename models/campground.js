6e13a93541ee60d030e7037cd7bfb9481ea18855
rishik-m <rishik.makhija2000@gmail.com>
2021-06-13 19:21:17 +0530

    Basic styling with node&ejs-mate

--- models/campground.js
+++ models/campground.js
@@ -6,6 +6,7 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    image: String
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
