51d7fbb403e38524a2b13a26f025d27a38a0852d
rishik-m <rishik.makhija2000@gmail.com>
2021-06-27 10:57:05 +0530

    login passport

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
 
@@ -12,12 +13,22 @@ router.post('/register', async (req, res) => {
         const user = new User({ email, username });
         const registerUser = await User.register(user, password);
         req.flash('success', "Welcome!!!!");
+        res.redirect('/campgrounds');
     }
     catch (e) {
         req.flash('success', e.message);
         res.redirect('register');
     }
 });
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', "Logged in successfully!!");
+    res.redirect('/campgrounds');
+})
 
 
 module.exports = router;
